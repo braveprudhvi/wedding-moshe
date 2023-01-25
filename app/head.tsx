b9b56a642b82273e6789820bc6ae50dec33b6208
braveprudhvi <prudhvi.8340@gmail.com>
2023-01-25 17:03:23 +0530

    Fixed Animations

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,6 @@ export default function Head() {
   return (
     <>
       <title>Prasanth Weds Archana</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Wedding Invitation of the couple Prasanth & Archana" />
       <link rel="icon" href="/image.ico" />
     </>
